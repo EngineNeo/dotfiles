5c0c843205be470e509bdf4f2351228d01f8f874
neom-fortune <neo.maralit@fortune.com>
2025-09-09 15:33:36 -0400

    Changed prompt for telescope to bottom

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
       layout_strategy = "horizontal",
       layout_config = {
         horizontal = {
-          prompt_position = "top",
+          prompt_position = "bottom",
           width = { padding = 0 },
           height = { padding = 0 },
           preview_width = 0.5,
