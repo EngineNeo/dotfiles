e3b01f8b263e9cd38931b04a26d14d0c12d95d67
neom-fortune <neo.maralit@fortune.com>
2025-09-19 16:14:52 -0400

    Updated lsp to use global

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,103 +1,91 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		local lspconfig = require("lspconfig")
-		local util = require("lspconfig.util")
-		lspconfig.ts_ls.setup({
-			filetypes = { "typescript", "typescriptreact", "javascript",
-				      "javascriptreact" }
-		})
+    "neovim/nvim-lspconfig",
+    config = function()
+        -- Global LSP configuration (applies to all servers)
+        vim.lsp.config('*', {
+            capabilities = vim.lsp.protocol.make_client_capabilities(),
+        })
 
-		--javascript, typescript, jsx, tsx, json, css, graphql, html
-		lspconfig.biome.setup({
-			cmd = function(dispatchers, config)
-				local cmd = "biome"
-				local local_cmd = (config or {}).root_dir and config.root_dir .. "/node_modules/.bin/biome"
-				if local_cmd and vim.fn.executable(local_cmd) == 1 then
-					cmd = local_cmd
-				end
-				return vim.lsp.rpc.start({ cmd, "lsp-proxy" }, dispatchers)
-			end,
-			filetypes = {
-				"astro",
-				"css",
-				"graphql",
-				"html",
-				"javascript",
-				"javascriptreact",
-				"json",
-				"jsonc",
-				"svelte",
-				"typescript",
-				"typescript.tsx",
-				"typescriptreact",
-				"vue",
-			},
-			workspace_required = true,
-			root_dir = function(fname)
-				return util.root_pattern("biome.json", "biome.jsonc")(fname)
-					or util.find_package_json_ancestor(fname)
-					or util.find_node_modules_ancestor(fname)
-					or util.find_git_ancestor(fname)
-			end,
-			-- root_dir = function(_, on_dir)
-			-- 	-- To support monorepos, biome recommends starting the search for the root from cwd
-			-- 	-- https://biomejs.dev/guides/big-projects/#use-multiple-configuration-files
-			-- 	local cwd = vim.fn.getcwd()
-			-- 	local root_files = { "biome.json", "biome.jsonc" }
-			-- 	root_files = util.insert_package_json(root_files, "biome", cwd)
-			-- 	local root_dir = vim.fs.dirname(vim.fs.find(root_files, { path = cwd, upward = true })[1])
-			-- 	on_dir(root_dir)
-			-- end,
-		})
-		-- vim.lsp.enable("biome")
+        -- TypeScript/JavaScript
+        vim.lsp.config.ts_ls = {
+            filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" }
+        }
+        vim.lsp.enable("ts_ls")
 
-		vim.lsp.enable("cssls")
-		vim.lsp.enable("graphql")
-		vim.lsp.enable("html")
-		vim.lsp.enable("tailwindcss", {
-			settings = {
-				tailwindCSS = {
-					experimental = {
-						classRegex = {
-							{ "([\"'`][^\"'`]*.*?[\"'`])", "[\"'`]([^\"'`]*).*?[\"'`]" },
-						},
-					},
-				},
-			},
-		})
+        -- Biome (JavaScript, TypeScript, CSS, etc.)
+        vim.lsp.config.biome = {
+            cmd = function()
+                local util = require("lspconfig.util")
+                local cmd = "biome"
+                local root_dir = util.root_pattern("biome.json", "biome.jsonc")(vim.fn.getcwd())
+                    or util.find_package_json_ancestor(vim.fn.getcwd())
+                    or util.find_node_modules_ancestor(vim.fn.getcwd())
+                    or util.find_git_ancestor(vim.fn.getcwd())
+                
+                local local_cmd = root_dir and root_dir .. "/node_modules/.bin/biome"
+                if local_cmd and vim.fn.executable(local_cmd) == 1 then
+                    cmd = local_cmd
+                end
+                return { cmd, "lsp-proxy" }
+            end,
+            filetypes = {
+                "astro", "css", "graphql", "html", "javascript", "javascriptreact",
+                "json", "jsonc", "svelte", "typescript", "typescript.tsx", 
+                "typescriptreact", "vue",
+            },
+            root_markers = { "biome.json", "biome.jsonc", "package.json", "node_modules", ".git" },
+        }
+        vim.lsp.enable("biome")
 
-		--bash
-		vim.lsp.enable("bashls")
-		vim.lsp.enable("shfmt")
-		vim.lsp.enable("shellcheck")
+        -- Web technologies
+        vim.lsp.enable("cssls")
+        vim.lsp.enable("graphql")
+        vim.lsp.enable("html")
+        
+        -- TailwindCSS
+        vim.lsp.config.tailwindcss = {
+            settings = {
+                tailwindCSS = {
+                    experimental = {
+                        classRegex = {
+                            { "([\"'`][^\"'`]*.*?[\"'`])", "[\"'`]([^\"'`]*).*?[\"'`]" },
+                        },
+                    },
+                },
+            },
+        }
+        vim.lsp.enable("tailwindcss")
 
-		-- c, c++
-		vim.lsp.enable("clangd")
-		vim.lsp.enable("clang-format")
-		vim.lsp.enable("cpplint")
+        -- Shell/Bash
+        vim.lsp.enable("bashls")
+        vim.lsp.enable("shfmt")
+        vim.lsp.enable("shellcheck")
 
-		--lua
-		vim.lsp.enable("lua_ls", {
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-				},
-			},
-		})
-		-- vim.lsp.enable("luacheck")
-		-- vim.lsp.enable("stylua")
+        -- C/C++
+        vim.lsp.enable("clangd")
+        vim.lsp.enable("clang-format")
+        vim.lsp.enable("cpplint")
 
-		--php
-		-- vim.lsp.enable("blade_formatter");
-		vim.lsp.enable("intelephense")
-		vim.lsp.enable("phpactor", {
-			filetypes = { "php", "blade" },
-		})
+        -- Lua
+        vim.lsp.config.lua_ls = {
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                },
+            },
+        }
+        vim.lsp.enable("lua_ls")
 
-		--other
-		vim.lsp.enable("typos_lsp")
-	end,
+        -- PHP
+        vim.lsp.enable("intelephense")
+        vim.lsp.config.phpactor = {
+            filetypes = { "php", "blade" },
+        }
+        vim.lsp.enable("phpactor")
+
+        -- Other
+        vim.lsp.enable("typos_lsp")
+    end,
 }
